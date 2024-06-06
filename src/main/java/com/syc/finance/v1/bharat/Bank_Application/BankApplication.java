35e890b1cf8663a1417ca5f132ed7942748925cc
saurabh kumar <saurabhaggarwal2017@gmail.com>
2024-06-06 15:54:44 +0530

    first commit

---
+++ src/main/java/com/syc/finance/v1/bharat/Bank_Application/BankApplication.java
@@ -0,0 +1,16 @@
+package com.syc.finance.v1.bharat.Bank_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BankApplication.class, args);
+
+		System.out.println("Banking application running.");
+	}
+
+}
